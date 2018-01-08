bf11bb36ca007db978665a09727adca59deab6b3
Adriano Gil <adrianogil.san@gmail.com>
2018-01-08 18:52:02 -0400

    Added bashrc files to ease installation

---
+++ bashrc.sh
@@ -0,0 +1,7 @@
+########## ADD TO Your BASHRC ##### Android Dev Config Files ########## 
+# Useful alias for Android development
+# export ANDROID_DEV_SCRIPTS_DIR=<PATH>
+# source ${ANDROID_DEV_SCRIPTS_DIR}/bashrc.sh
+
+source ${ANDROID_DEV_SCRIPTS_DIR}/android_dev.sh
+source ${ANDROID_DEV_SCRIPTS_DIR}/vr_android_dev.sh
