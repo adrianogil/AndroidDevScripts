6b09f6a949767734867ec29d38c2a13f2bb51c10
Adriano Gil <adrianogil.san@gmail.com>
2020-02-23 14:32:20 -0400

    Added easy way to call droid commands using sk-powered d function

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,16 @@
-########## ADD TO Your BASHRC ##### Android Dev Config Files ########## 
+########## ADD TO Your BASHRC ##### Android Dev Config Files ##########
 # Useful alias for Android development
 # export ANDROID_DEV_SCRIPTS_DIR=<PATH>
 # source ${ANDROID_DEV_SCRIPTS_DIR}/bashrc.sh
 
 source ${ANDROID_DEV_SCRIPTS_DIR}/android_dev.sh
 source ${ANDROID_DEV_SCRIPTS_DIR}/vr_android_dev.sh
+
+# @tool gt-sk
+function droid-sk()
+{
+    droid_action=$(cat ${ANDROID_DEV_SCRIPTS_DIR}/*android*.sh | grep '# droidtool' | cut -c12- | sk)
+
+    eval ${droid_action}
+}
+alias d="droid-sk"
